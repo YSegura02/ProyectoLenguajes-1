e5be8c780465ccbdd28f3e98b4e5a52bce25f7ae
glendaperez <appwebc05925@gmail.com>
2024-05-06 22:34:20 -0600

    create
    creacion controller para Cliente

---
+++ ProyectoHotel#1/ProyectoHotel#1/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoHotel_1.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
