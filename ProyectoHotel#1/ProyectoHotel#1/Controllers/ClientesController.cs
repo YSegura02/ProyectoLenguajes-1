cfd6d31d4b288d2440fae465658fd06e3376bcbb
YSegura02 <92764997+YSegura02@users.noreply.github.com>
2024-05-06 22:53:58 -0600

    Cambios a Cliente

---
+++ ProyectoHotel#1/ProyectoHotel#1/Controllers/ClientesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoHotel_1.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ProyectoHotel#1/ProyectoHotel#1/obj/Debug/net8.0/ProyectoHotel#1.AssemblyInfo.cs
+++ ProyectoHotel#1/ProyectoHotel#1/obj/Debug/net8.0/ProyectoHotel#1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoHotel#1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e144968a6a1fa2dda448cc786b61835101d3784f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6a04ddbef74864c5a4e50c7bfc41761b236faf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoHotel#1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoHotel#1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
