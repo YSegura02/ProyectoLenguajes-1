f6a04ddbef74864c5a4e50c7bfc41761b236faf6
YSegura02 <92764997+YSegura02@users.noreply.github.com>
2024-05-06 22:28:29 -0600

    Creación cliente

---
+++ ProyectoHotel#1/ProyectoHotel#1/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿namespace ProyectoHotel_1.Models
+{
+    public class Cliente
+    {
+        public int Cedula { get; set; }
+
+        public string TipoCedula { get; set; }
+
+        public string NombreCompleto { get; set; }
+
+        public int Telefono { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string email { get; set; }
+
+    }
+}
--- ProyectoHotel#1/ProyectoHotel#1/obj/Debug/net8.0/ProyectoHotel#1.AssemblyInfo.cs
+++ ProyectoHotel#1/ProyectoHotel#1/obj/Debug/net8.0/ProyectoHotel#1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoHotel#1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10a20636be621c3b11b04af3de5b74098ff34527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e144968a6a1fa2dda448cc786b61835101d3784f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoHotel#1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoHotel#1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
